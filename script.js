28eedb45ff9925f794e00e286d417ee0df4a6869
Pratham Kamble <91152849+PrathamKamble@users.noreply.github.com>
2023-11-09 22:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Pink Floyd'];
 
+// Function to remove articles from band names
+function removeArticles(name) {
+    return name.replace(/^(a|an|the)\s+/i, '');
+}
+
+// Sorting the band names without articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Creating a list and adding band names to it
+let bandList = document.getElementById('band');
+bandNames.forEach(name => {
+    let listItem = document.createElement('li');
+    listItem.textContent = name;
+    bandList.appendChild(listItem);
+});
